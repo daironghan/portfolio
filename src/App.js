0931bc6af25da1bae9123d94548ba905ab7e9409
daironghan <rongrong910@gmail.com>
2024-12-19 10:59:16 -0800

    routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route, HashRouter } from "react-router-dom";
 import './App.css';
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div className="app">
-      App
-    </div>
+      <div className="app">
+        App
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
   );
 }
 
